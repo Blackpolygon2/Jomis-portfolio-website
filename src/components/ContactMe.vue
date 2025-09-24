4b6fb34172fc9d7bb25881d03dd13e02564bc34a
Blackpolygon2 <yobiojomi@gmail.com>
2025-09-24 16:05:19 -0400

    intial commit

---
+++ src/components/ContactMe.vue
@@ -0,0 +1,13 @@
+
+<template>
+  <div >
+     <font-awesome-icon icon="fa-solid fa-paper-plane" />
+    <p>Contact me</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+</script>
---
+++ src/components/testcomp.vue
@@ -0,0 +1,12 @@
+
+<template>
+  <p class="greeting">{{ greeting }}</p>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const greeting = ref('Hello World!')
+</script>
+
+
+
